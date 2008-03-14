b4fdd48e8ece7a9233617bb6bb9f79d5a865eaab
Fedor A. Fetisov <faf@ossg.ru>
2008-03-14 20:51:47 +0000

    Added handler for SIGUSR1 - to reopen logs for log rotating
    git-svn-id: svn://svn/open/alice/trunk@118 da265d1a-f118-0410-bc39-86f197818eeb

--- alice.pl
+++ alice.pl
@@ -86,10 +86,7 @@ for my $handle (*STDIN, *STDOUT) {
     }
 }
 
-unless (open(*STDERR, '>>', $config->{'service'}->{'error_log'})) {
-    print STDERR "Can't reopen STDERR to $config->{'service'}->{'error_log'} : $!\n";
-    _disconnect();
-}
+_open_log();
 
 unless (POSIX::setsid()) {
     print STDERR "Can't start a new session: $!\n";
@@ -99,6 +96,7 @@ unless (POSIX::setsid()) {
 
 $SIG{HUP} = $SIG{KILL} = $SIG{TERM} = $SIG{INT} = \&_disconnect;
 $SIG{PIPE} = 'IGNORE';
+$SIG{USR1} = \&_reopen_log;
 
 my $reconnect_flag = ($config->{'component_connection'}->{'reconnect'} &&
 		    ($config->{'component_connection'}->{'reconnect'} ne 'no')) ? 1 : 0;
@@ -273,4 +271,16 @@ sub _disconnect {
     $alice->Disconnect() if defined $alice;
     unlink($config->{'service'}->{'pid_file'}) if (-f $config->{'service'}->{'pid_file'});
     exit;
+}
+
+sub _reopen_log {
+    open(*STDERR, '+<', '/dev/null');
+    _open_log();
+}
+
+sub _open_log {
+    unless (open(*STDERR, '>>', $config->{'service'}->{'error_log'})) {
+	print STDERR "Can't reopen STDERR to $config->{'service'}->{'error_log'} : $!\n";
+	_disconnect();
+    }
 }
